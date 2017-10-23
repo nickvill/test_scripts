435f4963f60fa302dfe1f5d272cdb3d5e65f80e6
nickvill <nickvill@mit.edu>
2017-10-22 21:59:20 -0400

    added object, instance, and queue

--- zed_listener.py
+++ zed_listener.py
@@ -2,14 +2,13 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
 
-class zed_listener:
-    
+class zed_listener(object):
+
     def __init__(self):
 
-        self.sub = rospy.Subscriber('/zed/point_cloud/cloud_registered', PointCloud2, self.callback)
+        self.sub = rospy.Subscriber('/zed/point_cloud/cloud_registered', PointCloud2, self.callback, queue_size=5)
 
         self.pub = rospy.Publisher('depth', PointCloud2, queue_size=5)
-        
 
     def callback(data):
         self.pub.publish(data)
@@ -17,4 +16,5 @@ class zed_listener:
 
 if __name__ == '__main__':
     rospy.init_node('listener', anonymous=True)
+    zed_list = zed_listener()
     rospy.spin()
