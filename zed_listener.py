d83685112c8bef36250f8a43c7690d7affa04707
nickvill <nickvill@mit.edu>
2017-10-22 21:41:41 -0400

    changed to class

--- zed_listener.py
+++ zed_listener.py
@@ -2,24 +2,18 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
 
-def callback(data):
-    pub.publish(data)
+class zed_listener:
+    
+    def __init__(self):
 
-def listener():
+        self.sub = rospy.Subscriber('/zed/depth/depth_registered', PointCloud2, callback)
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+        self.pub = rospy.Publisher('depth', PointCloud2, queue_size=5)
+        
 
-    rospy.Subscriber('/zed/depth/depth_registered', PointCloud2, callback)
-
-    pub = rospy.Publisher('depth', PointCloud2, queue_size=5)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    def callback(data):
+        self.pub.publish(data)
 
 if __name__ == '__main__':
-    listener()
+    rospy.init_node('listener', anonymous=True)
+    rospy.spin()
