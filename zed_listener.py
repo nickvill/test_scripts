827b29476f27e49f4429e684dff782f014238e61
nickvill <nickvill@mit.edu>
2017-10-25 17:18:47 -0400

    pub imgs

--- zed_listener.py
+++ zed_listener.py
@@ -5,34 +5,44 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+import tf
+from tf2_msgs import TFMessage
 
 
 class zed_listener(object):
 
     def __init__(self):
 
-        # self.sub = rospy.Subscriber('/zed/point_cloud/cloud_registered', PointCloud2, self.callback, queue_size=5)
+        # self.sub = rospy.Subscriber('/zed/point_cloud/cloud_registered', PointCloud2, self.pc_callback, queue_size=5)
         self.sub = rospy.Subscriber('/zed/rgb/image_raw_color', Image, self.callback, queue_size=5)
-
-        self.pub = rospy.Publisher('depth', PointCloud2, queue_size=5)
+        # self.tfsub = rospy.Subscriber('/tf', TFMessage, self.broadcast, queue_size = 1)
+        self.pc_pub = rospy.Publisher('depth', PointCloud2, queue_size=5)
+        self.img_pub = rospy.Publisher('zed_img', Image, queue_size=1)
         self.bridge = CvBridge()
 
-    def callback(self, msg):
+    def pc_callback(self, msg):
 
-        # self.pub.publish(msg)
-        # print "pubbed"
+        self.pub.publish(msg)
+        print "pubbed pt cloud"
+
+
+
+    def img_callback(self, msg):
 
         try:
             cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
         except CvBridgeError as e:
             print e
 
-        height, width, channels = cv_image.shape
+        self.img_pub.publish(msg)
+        print 'pubbed img'
+        # height, width, channels = cv_image.shape
 
-        print 'width: ', width, 'height: ', height
-        
-        
+        # print 'width: ', width, 'height: ', height
 
+
+        
+    
 if __name__ == '__main__':
     rospy.init_node('listener', anonymous=True)
     zed_list = zed_listener()
